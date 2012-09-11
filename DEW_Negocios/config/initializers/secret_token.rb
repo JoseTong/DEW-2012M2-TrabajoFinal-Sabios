e83c1d7efb5069c9900e517ad79b3561418e01d3
JoseTong <jtong@grupogss.com>
2012-09-10 22:23:05 -0500

    inicializa estructura

--- DEW_Negocios/config/initializers/secret_token.rb
+++ DEW_Negocios/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DEWNegocios::Application.config.secret_token = 'dd0826916007874a0572e83b9e045a0b4148a14012f524b076427507b6e47b38486ad105af9f85d93e819d7fc3959a5763a8669c31932f52dcd2dea0d6289061'
+DEWNegocios::Application.config.secret_token = '33acaf88c2b75c5396989655acdb67064efb63728ccc97e70fda60f866f2f33b2242e44ce2999765518d8b3dc4ce833fffd30ce9885898662cd06fa8d08b3dc5'
